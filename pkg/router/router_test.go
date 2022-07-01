89bfc49f7352762bd9e42e9dfc012d7d404e483b
Codexiaoyi <874183200@qq.com>
2022-07-01 19:02:52 +0800

    add router unit test

--- pkg/router/router_test.go
+++ pkg/router/router_test.go
@@ -14,38 +14,37 @@
 
 package router
 
-// import (
-// 	"linweb/interfaces"
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/stretchr/testify/assert"
+)
 
-// func newTestRouter() *Router {
-// 	r := &Router{root: make(map[string]*node), handlers: make(map[string]interfaces.HandlerFunc)}
-// 	r.addRoute("GET", "/", nil)
-// 	r.addRoute("GET", "/hello/:name", nil)
-// 	r.addRoute("GET", "/hello/b/c", nil)
-// 	r.addRoute("GET", "/hi/:name", nil)
-// 	r.addRoute("GET", "/assets/*filepath", nil)
-// 	return r
-// }
+func newTestRouter() *Router {
+	r := &Router{root: make(map[MethodType]*node), handlers: make(map[string]*Function)}
+	r.addRoute(GET, "/", nil)
+	r.addRoute(GET, "/hello/:name", nil)
+	r.addRoute(GET, "/hello/b/c", nil)
+	r.addRoute(GET, "/hi/:name", nil)
+	r.addRoute(GET, "/assets/*filepath", nil)
+	return r
+}
 
-// func TestParsePattern(t *testing.T) {
-// 	assert.Equal(t, []string{"p", ":name"}, parsePattern("/p/:name"))
-// 	assert.Equal(t, []string{"p", "*"}, parsePattern("/p/*"))
-// 	assert.Equal(t, []string{"p", "*name"}, parsePattern("/p/*name/*"))
-// }
+func TestParsePattern(t *testing.T) {
+	assert.Equal(t, []string{"p", ":name"}, parsePattern("/p/:name"))
+	assert.Equal(t, []string{"p", "*"}, parsePattern("/p/*"))
+	assert.Equal(t, []string{"p", "*name"}, parsePattern("/p/*name/*"))
+}
 
-// func TestGetRoute(t *testing.T) {
-// 	router := newTestRouter()
-// 	node, params := router.getRoute("GET", "/hello/linweb")
-// 	assert.NotEmpty(t, node)
-// 	assert.Equal(t, "/hello/:name", node.url)
-// 	assert.Equal(t, "linweb", params["name"])
+func TestGetRoute(t *testing.T) {
+	router := newTestRouter()
+	node, params := router.getRoute(GET, "/hello/linweb")
+	assert.NotEmpty(t, node)
+	assert.Equal(t, "/hello/:name", node.url)
+	assert.Equal(t, "linweb", params["name"])
 
-// 	node1, params1 := router.getRoute("GET", "/assets/go/linweb/router.go")
-// 	assert.NotEmpty(t, node1)
-// 	assert.Equal(t, "/assets/*filepath", node1.url)
-// 	assert.Equal(t, "go/linweb/router.go", params1["filepath"])
-// }
+	node1, params1 := router.getRoute(GET, "/assets/go/linweb/router.go")
+	assert.NotEmpty(t, node1)
+	assert.Equal(t, "/assets/*filepath", node1.url)
+	assert.Equal(t, "go/linweb/router.go", params1["filepath"])
+}
